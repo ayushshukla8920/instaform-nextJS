4066e6c5c873ecef0caa6120cc5e7f05b5f2252f
Ayush Shukla <149079402+ayushshukla8920@users.noreply.github.com>
2025-03-25 10:32:58 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div className='bg-black text-white text-3xl font-semibold'>
+    <div className='bg-black text-white text-3xl font-semibold w-full min-h-screen'>
       This website is under Maintenance 
     </div>
   )

10b6d4093605e080d0f409d04681fbf23db01994
Ayush Shukla <ayushshukla8920@gmail.com>
2025-03-27 11:12:29 +0530

    Changed LLM model to an Advanced One

--- app/api/ai/generate-content/route.js
+++ app/api/ai/generate-content/route.js
@@ -2,7 +2,7 @@ import connectToDB from "@/lib/db";
 import { NextResponse } from "next/server"
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 const user = require('@/models/user');
 const form = require('@/models/Form');
 const jwt = require('jsonwebtoken');
